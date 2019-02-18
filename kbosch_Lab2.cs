277d381aa5e968ac68aab58d122fa9dc6d62dd0d
KT <kbosch@ramapo.edu>
2019-02-18 13:29:20 -0500

    Lab 2 done
    Problem is commented at the top

--- kbosch_Lab2.cs
+++ kbosch_Lab2.cs
@@ -14,7 +14,44 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            double a = -200;
+            double b = 300;
+            double root = -1;
 
+            while (root == -1)
+            {
+                //Find middle point
+                double c = (a + b) / 2;
+
+                //Verify root
+                if (equation(c) == 0.0)
+                {
+                    root = c;
+                }
+                else if (equation(a)*equation(c) < 0)
+                {
+                    b = c;
+                }
+                else if (equation(b)*equation(c) < 0)
+                {
+                    a = c;
+                }
+                else
+                {
+                    Console.WriteLine("Error: function doesn't follow an assumption of the bisection method");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            Console.WriteLine("Root: " + root);
+            Console.ReadKey();
+            return;
+        }
+
+        static double equation(double x)
+        {
+            //y = x^3 - 2
+            return (x * x * x) - 2;
         }
     }
 }
